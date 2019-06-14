521c721bf455b2212d75c8f3729eb2ba926d13b2
scaraclette <gustiscarlett@gmail.com>
2019-06-14 09:08:19 -0700

    Revert "socket working, next step to fix channel changes"
    This reverts commit 185356aa1469e5fd5a425bef20125fd1a8056626.

--- static/chat.js
+++ static/chat.js
@@ -1,45 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //Initially, clear storage for debugging. Later create a function
-    //Todo: only delete keys that are not available in get channels
-    localStorage.clear();
 
     // By default the creating channel and typing message are disabled
     defaultDisable();    
 
     // ******** Current user **************
-    curUsr();
-
-    // ********* Chat page *****************
-
-    // ********* TEST ***************
-    // testChat();
-
-
-});
-
-// Function that returns messages
-// function testChat() {
-//     const request = new XMLHttpRequest();
-//     request.open('GET', '/chat-msg');
-
-//     request.onload = () => {
-//         const data = JSON.parse(request.responseText);
-//         console.log("data: " + data['default'][0]['user']);
-//     }
-
-//     request.send();
-// }
-
-// Disable others
-function defaultDisable() {
-    document.querySelector('#channelInput').disabled = true;
-    document.querySelector('#channelSubmit').disabled = true;
-    document.querySelector('#chatInput').disabled = true;
-    document.querySelector('#chatSubmit').disabled = true;
-}
-
-// Function that gets current user to use for localStorage later
-function curUsr() {
     disableButton('#userSubmit', '#userInput');
     document.querySelector('#enterUser').onsubmit = () => {
         document.querySelector('#channelInput').disabled = false;
@@ -58,6 +22,34 @@ function curUsr() {
         data.append("crUser", currentUser);
         request.send(data)
         return false;
+    }
+
+    // ********* Chat page *****************
+
+    // ********* TEST ***************
+
+
+});
+
+// Disable others
+function defaultDisable() {
+    document.querySelector('#channelInput').disabled = true;
+    document.querySelector('#channelSubmit').disabled = true;
+    document.querySelector('#chatInput').disabled = true;
+    document.querySelector('#chatSubmit').disabled = true;
+}
+
+// Function that gets current user to use for localStorage later
+function curUsr() {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/current-user');
+    request.send(null);
+
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        console.log(data)
+        let currentUser = data;
+        document.querySelector('#currentUser').innerHTML = "Current user: " + currentUser;
     }
 
 }
@@ -132,23 +124,9 @@ function getChannels() {
 function openChannel(chnName) {
     // Update localStorage onclick
     userLocalStorage(chnName);
+
     console.log("CURRENT LOCAL STORAGE");
     console.log(localStorage)
-    // Delete previous messageList items
-    deleteMl();
-    // Get current messages with Ajax requests
-    const request = new XMLHttpRequest();
-    request.open('GET', '/get-chat')
-
-    request.onload = () => {
-        const data = JSON.parse(request.responseText);
-        console.log("DATA: " + data);
-    }
-
-    const data = new FormData();
-    data.append("chnName", chnName);
-    request.send(data)
-    
     // Enable submit button for chat
     document.querySelector('#chatInput').disabled = false;
     disableButton('#chatSubmit', '#chatInput');
@@ -158,59 +136,7 @@ function openChannel(chnName) {
 
     document.querySelector('#currentChannel').innerHTML = "Current channel: " + chnName;
 
-    /**
-     * SOCKET.IO, want to send {channel name: [user, message, time]}
-     */
-    //TODO: Start WebSocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    // When connected, send message
-    socket.on('connect', () => {
-        document.querySelector('#newChat').onsubmit = () => {
-            let msg = document.querySelector('#chatInput').value;
-            console.log("CURRENT MESSAGE: " + msg);
-
-            // Get current timestamp
-            let today = new Date();
-            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-            let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            let dateTime = date + ' ' + time;
-
-            // Send message and chnName
-            socket.emit('submit chat', {'chnName': chnName, 'msg':msg, 'dateTime':dateTime})
-            return false;
-        }
-    });
-
-    // When chat is received, update to '#messageList'
-    socket.on('show chat', getChat => {
-        // An example of getting a message value: getChat[0]['msg']
-        // For debugging purposes, keep track of count in console
-        let count = 1;
-        // Delete the contents of messageList first
-        deleteMl();
-
-        // Update message list first
-        getChat.forEach(function(index) {
-            // Debugging purposes
-            console.log(count + ": " + index['user'] + ": " + index['msg'] + " (" + index['dateTime'] + ")");
-            count++;
-
-            let node = document.createElement('li');
-            let textNode = document.createTextNode(index['user'] + " (" + index['dateTime'] + "): " + index['msg']);
-            node.appendChild(textNode);
-            document.getElementById('messageList').appendChild(node);
-        });
-    });
-
-}
-
-function deleteMl() {
-    // Delete the contents of messageList first
-    let ml = document.querySelector('#messageList');
-    while (ml.firstChild) {
-        ml.removeChild(ml.firstChild);
-    }
+    // Start WebSocket
 }
 
 function userLocalStorage(chnName) {
